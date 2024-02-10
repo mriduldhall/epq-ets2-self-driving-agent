4bc8cc20fa780f84c3c647f49fd7e78b6d028710
mriduldhall <mriduldhall1@gmail.com>
2024-02-10 15:24:45 +0000

    Refactor screenshot into oop

--- car_detectors/screenshot.py
+++ car_detectors/screenshot.py
@@ -1,14 +1,12 @@
 import cv2
 import numpy as np
-from time import time
 from pyautogui import screenshot
 
 
-if __name__ == '__main__':
-    start_time = time()
-    image = screenshot(region=(0, 0, 300, 400))
-    end_time = time()
-    print(f"Screenshot took {end_time - start_time} seconds.")
-    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-    cv2.imshow('screenshot', image)
-    cv2.waitKey(0)
+class Screenshot:
+    def __init__(self, region=(500, 300, 400, 225)):
+        self.region = region
+
+    def take_screenshot(self):
+        image = screenshot(region=self.region)
+        return cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
