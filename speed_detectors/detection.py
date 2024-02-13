b990486a990efd3c8e1a3d0cb8340ea8c90f9f30
mriduldhall <mriduldhall1@gmail.com>
2024-02-13 22:40:56 +0000

    Refactor check_similarity method to check_exact_match

--- speed_detectors/detection.py
+++ speed_detectors/detection.py
@@ -9,16 +9,16 @@ class Detector:
         self.screenshot = Screenshot()
 
     @staticmethod
-    def check_similarity(image_one, image_two):
+    def check_exact_match(image_one, image_two):
         return image_one.shape == image_two.shape and not (np.bitwise_xor(image_one, image_two).any())
 
     def detect_speed_limit(self):
         image = self.screenshot.take_speed_limit_screenshot()
-        if self.check_similarity(image, cv2.imread('speed_limits/60.png')):
+        if self.check_exact_match(image, cv2.imread('speed_limits/60.png')):
             return 60
-        if self.check_similarity(image, cv2.imread('speed_limits/50.png')):
+        if self.check_exact_match(image, cv2.imread('speed_limits/50.png')):
             return 50
-        if self.check_similarity(image, cv2.imread('speed_limits/30.png')):
+        if self.check_exact_match(image, cv2.imread('speed_limits/30.png')):
             return 30
 
     def detect_speed_tesseract(self):
