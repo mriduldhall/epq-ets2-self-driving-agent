e3e9a0490d30677d6e54be79ffe5efcadbecc6a5
mriduldhall <mriduldhall1@gmail.com>
2024-02-13 15:03:33 +0000

    Detect speed using tesseract

--- speed_detectors/detection.py
+++ speed_detectors/detection.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import pytesseract
 from car_detectors.screenshot import Screenshot
 
 
@@ -19,3 +20,7 @@ class Detector:
             return 50
         if self.check_similarity(image, cv2.imread('speed_limits/30.png')):
             return 30
+
+    def detect_speed_tesseract(self):
+        img = self.screenshot.take_speed_screenshot()
+        return pytesseract.image_to_string(img, lang='eng', config='--psm 6 -c tessedit_char_whitelist=0123456789')
