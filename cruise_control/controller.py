43a88497e6fe8d66db1bcb698555038955c4971a
mriduldhall <mriduldhall1@gmail.com>
2024-02-14 14:44:45 +0000

    Declare keybinds in controller class

--- cruise_control/controller.py
+++ cruise_control/controller.py
@@ -5,16 +5,19 @@ from pyautogui import hold, press
 class Controller:
     def __init__(self):
         self.differential = 0
+        self.differential_up = ";"
+        self.differential_down = "'"
+        self.accelerate = "w"
 
     def control(self, command):
-        press("'", presses=4, interval=0.05)
+        press(self.differential_down, presses=4, interval=0.05)
         current_command = command.get_command()
         while True:
             if current_command == "N":
                 sleep(0.2)
                 current_command = command.get_command()
             elif current_command == "A":
-                with hold("w"):
+                with hold(self.accelerate):
                     while current_command == "A":
                         sleep(0.2)
                         current_command = command.get_command()
