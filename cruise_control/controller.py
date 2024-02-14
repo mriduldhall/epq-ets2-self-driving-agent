20442a16fb14e62476ee0d1bc19302a517a47df1
mriduldhall <mriduldhall1@gmail.com>
2024-02-14 14:43:09 +0000

    Ensure differential braking is turned off on start

--- cruise_control/controller.py
+++ cruise_control/controller.py
@@ -1,12 +1,13 @@
 from time import sleep
-from pyautogui import hold
+from pyautogui import hold, press
 
 
 class Controller:
     def __init__(self):
-        pass
+        self.differential = 0
 
     def control(self, command):
+        press("'", presses=4, interval=0.05)
         current_command = command.get_command()
         while True:
             if current_command == "N":
