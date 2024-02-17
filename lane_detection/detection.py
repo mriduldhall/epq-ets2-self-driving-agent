4c7d58230486ba8cf0c784ad7afacf69f952e955
mriduldhall <mriduldhall1@gmail.com>
2024-02-17 16:44:14 +0000

    Refactor detect method to only return single image

--- lane_detection/detection.py
+++ lane_detection/detection.py
@@ -101,24 +101,8 @@ class Detector:
         detected_lanes_image = cv2.warpPerspective(color_warp, inverse_matrix, self.image_size)
         return detected_lanes_image
 
-    def detect(self):
-        images = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        # images = ["10", "11", "12", "13", "14", "15", "16", "17", "18", "19"]
-        counter = 0
-        for image in images:
-            image = cv2.imread('../Test-Data/Lane Detection/field_of_view_' + image + '.png')
-            thresholded, inverse_matrix = self.process_image(image)
-            left_fit, right_fit = self.detect_lanes_sliding_window(thresholded)
-            detected = self.draw_lane(thresholded, left_fit, right_fit, inverse_matrix)
-            if counter == 0:
-                numpy_horizontal_concat = detected
-            else:
-                numpy_horizontal_concat = np.concatenate((numpy_horizontal_concat, detected), axis=1)
-            counter += 1
-        cv2.imshow('Detected', numpy_horizontal_concat)
-        cv2.waitKey(0)
-
-
-if __name__ == '__main__':
-    detector = Detector()
-    detector.detect()
+    def detect(self, image):
+        thresholded, inverse_matrix = self.process_image(image)
+        left_fit, right_fit = self.detect_lanes_sliding_window(thresholded)
+        detected = self.draw_lane(thresholded, left_fit, right_fit, inverse_matrix)
+        return detected
