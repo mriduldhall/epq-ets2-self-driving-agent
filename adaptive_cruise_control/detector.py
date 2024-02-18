a6b29484c6f68d995b52fffb7c86c9efbaa6f6b2
mriduldhall <mriduldhall1@gmail.com>
2024-02-18 00:05:04 +0000

    Filter vehicles in the current lane

--- adaptive_cruise_control/detector.py
+++ adaptive_cruise_control/detector.py
@@ -8,6 +8,32 @@ class Detector:
         self.speed_detector = SpeedDetector()
         self.screenshot = Screenshot()
 
+    @staticmethod
+    def find_applicable_vehicle(detected_vehicles, detected_lane_image):
+        applicable_vehicles = []
+        closest_vehicle = []
+        for vehicle in detected_vehicles:
+            y_position = vehicle[0][1] + vehicle[0][3]
+            x_position = vehicle[0][0] + (vehicle[0][2] / 2)
+            column = detected_lane_image[y_position]
+            left_lane = None
+            right_lane = None
+            pixel = 0
+            while pixel < len(column) and left_lane is None:
+                if column[pixel].any():
+                    left_lane = pixel
+                pixel += 1
+            pixel = len(column) - 1
+            while pixel >= 0 and right_lane is None:
+                if column[pixel].any():
+                    right_lane = pixel
+                pixel -= 1
+            if left_lane is not None and right_lane is not None and left_lane <= x_position <= right_lane:
+                applicable_vehicles.append(vehicle)
+                if len(closest_vehicle) == 0 or vehicle[1] < closest_vehicle[1]:
+                    closest_vehicle = [vehicle]
+        return closest_vehicle
+
     def engage(self, command):
         while True:
             start_time = time()
