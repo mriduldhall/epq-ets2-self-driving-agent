49f9cc822ce7831cda32dc01e2e2188e9e2acc12
mriduldhall <mriduldhall1@gmail.com>
2024-02-20 22:04:59 +0000

    Fix bug where incorrect lock was being released

--- adaptive_cruise_control/command.py
+++ adaptive_cruise_control/command.py
@@ -6,15 +6,14 @@ class Command:
         self.command = "N"
         self.detector_lock = Lock()
         self.controller_lock = Lock()
-        self.detector_lock.acquire()
 
     def set_command(self, command):
-        self.controller_lock.acquire()
+        self.detector_lock.acquire()
         self.command = command
         self.detector_lock.release()
 
     def get_command(self):
-        self.detector_lock.acquire()
+        self.controller_lock.acquire()
         command = self.command
         self.controller_lock.release()
         return command
