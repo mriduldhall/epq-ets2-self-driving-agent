3262cd91ddc9ad422055f0e968fc4773f5bd4658
mriduldhall <mriduldhall1@gmail.com>
2024-02-20 22:05:32 +0000

    Add support for engine and air brake commands

--- adaptive_cruise_control/controller.py
+++ adaptive_cruise_control/controller.py
@@ -8,6 +8,9 @@ class Controller:
         self.differential_up = ";"
         self.differential_down = "'"
         self.accelerate = "w"
+        self.engine_brake = "b"
+        self.air_brake = "s"
+        self.sleep_time = 0.5
 
     def apply_differential(self, differential):
         increments = int(differential) - self.differential
@@ -22,16 +25,31 @@ class Controller:
         current_command = command.get_command()
         while True:
             if current_command == "N":
-                sleep(0.2)
+                sleep(self.sleep_time)
                 current_command = command.get_command()
             elif current_command == "A":
                 with hold(self.accelerate):
                     while current_command == "A":
-                        sleep(0.2)
+                        sleep(self.sleep_time)
                         current_command = command.get_command()
             elif current_command in ("1", "2", "3", "4"):
                 self.apply_differential(current_command)
-                sleep(0.2)
+                sleep(self.sleep_time)
                 current_command = command.get_command()
                 if current_command not in ("1", "2", "3", "4"):
                     self.apply_differential(0)
+            elif current_command == "E":
+                with hold(self.engine_brake):
+                    self.apply_differential("4")
+                    while current_command == "E":
+                        sleep(self.sleep_time)
+                        current_command = command.get_command()
+                    self.apply_differential(0)
+            elif current_command == "B":
+                with hold(self.air_brake):
+                    with hold(self.engine_brake):
+                        self.apply_differential("4")
+                        while current_command == "B":
+                            sleep(self.sleep_time)
+                            current_command = command.get_command()
+                        self.apply_differential(0)
