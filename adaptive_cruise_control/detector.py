1363f40921cfe83b7a1f78ec53df50c9557c5656
mriduldhall <mriduldhall1@gmail.com>
2024-02-20 22:07:16 +0000

    Calculate relative speed using detected vehicle in lane

--- adaptive_cruise_control/detector.py
+++ adaptive_cruise_control/detector.py
@@ -1,12 +1,19 @@
 from time import time, sleep
 from car_detectors.screenshot import Screenshot
+from lane_detection.detection import Detector as LaneDetector
 from speed_detectors.detection import Detector as SpeedDetector
+from car_detectors.yolo.detector import Detector as VehicleDetector
 
 
 class Detector:
     def __init__(self):
         self.speed_detector = SpeedDetector()
+        self.vehicle_detector = VehicleDetector()
+        self.lane_detector = LaneDetector()
         self.screenshot = Screenshot()
+        self.previous_vehicle_distance = None
+        self.second_previous_vehicle_distance = None
+        self.minimum_distance = 25
 
     @staticmethod
     def find_applicable_vehicle(detected_vehicles, detected_lane_image):
@@ -35,6 +42,26 @@ class Detector:
                 if len(closest_vehicle) == 0 or vehicle[1] < closest_vehicle[0][1]:
                     closest_vehicle = [vehicle]
         return closest_vehicle
+
+    def calculate_relative_speed(self, vehicle, time_difference=1):
+        if len(vehicle) == 0:
+            return None
+        vehicle = vehicle[0]
+        if self.previous_vehicle_distance is None and self.second_previous_vehicle_distance is None:
+            return None
+        if self.previous_vehicle_distance is None:
+            distance_change = self.second_previous_vehicle_distance - float(vehicle[1])
+            return distance_change / (2 * time_difference)
+        distance_change = self.previous_vehicle_distance - float(vehicle[1])
+        return distance_change / time_difference
+
+    def update_previous_distances(self, vehicle):
+        if len(vehicle) == 0:
+            distance = None
+        else:
+            distance = float(vehicle[0][1])
+        self.second_previous_vehicle_distance = self.previous_vehicle_distance
+        self.previous_vehicle_distance = distance
 
     def engage(self, command):
         while True:
